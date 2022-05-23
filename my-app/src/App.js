ab85cdf43ac775fd43a72ac3e964737e3788884e
Debaditya <ddev2636@gmail.com>
2022-05-23 12:15:52 +0530

    navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,17 @@
-export default function App() {
+
+import navbar from "./components/navbar";
+function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      Hello world!me
+      
+    </h1>;
+    
+
+    <navbar/>
+    
+  </>
   )
 }
+export default App;
---
+++ my-app/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const navbar = () => {
+  return (
+    <>      
+<h1>hello</h1>
+    </>
+  )
+}
+
+export default navbar
